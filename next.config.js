6012d53e7aa18ea008a23be99d3987dc6ad62364
ore291 <oreoluwapadonu@gmail.com>
2023-02-10 23:28:10 +0100

    .

--- next.config.js
+++ next.config.js
@@ -13,6 +13,18 @@ module.exports = withBundleAnalyzer({
   poweredByHeader: false,
   trailingSlash: false,
   basePath: '',
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
