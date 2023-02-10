01a3f16a9990de0c957e2f8509a7d771c2a8f0d0
ore291 <oreoluwapadonu@gmail.com>
2023-02-10 23:21:53 +0100

    .

---
+++ types/next-auth.d.ts
@@ -0,0 +1,27 @@
+import NextAuth, { DefaultSession } from "next-auth"
+
+declare module "next-auth" {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {
+      username: string;
+      email: string;
+      password: string;
+      location?: string;
+      phone?: string;
+      role: string;
+      isEmailVerified: boolean;
+      firstname: String; // firstName
+      lastname: String; // lastName
+      bio: String; // A new bio
+      dob: String; // 23rd july 2018
+      profile_pic: String | null;
+      lastLogin: String | null;
+      followers: any;
+      following: any;
+      requests: any;
+    } & DefaultSession["user"]
+  }
+}
